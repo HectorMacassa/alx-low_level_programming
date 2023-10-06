163e280f688ea3c5ab897634f1efc27b541a69c6
Hector Macassa <cbmacassa@gmail.com>
2023-10-05 18:20:55 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - prints a phrase and adds a new line at the end
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
