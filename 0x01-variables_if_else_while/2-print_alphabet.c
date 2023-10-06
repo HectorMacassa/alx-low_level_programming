feedf155993411de4f87a0cab4fc3d2c3bff900b
Hector Macassa <cbmacassa@gmail.com>
2023-10-06 12:58:13 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stio.h>
+
+/**
+ * main - this functin prints letters of the alphabet
+ * Return: 0
+ */
+int main (void)
+{
+	char letter = 'a';
+	while (letter <= 'z');
+	{
+		putchar(letter);
+		letter ++;
+	}
+	putchar('\n');
+	return (0);
+}
