8659080920883f46d8a97938f48c14f27ca72034
Hector Macassa <cbmacassa@gmail.com>
2023-10-05 18:38:38 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a phrase and adds a new line at the end
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
