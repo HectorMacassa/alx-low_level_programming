c36f38b3841c403d95caf94efa7a0dace1bc430b
Hector Macassa <cbmacassa@gmail.com>
2023-10-07 01:53:16 -0700

    tebahplA

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - reverse prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n);
+	return (0);
+}
