3f3b9d8bcb0c2f8a0adeee93186469875b74f521
Hector Macassa <cbmacassa@gmail.com>
2023-10-10 20:59:15 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - prints the alphabet
- * Return: On success 1
+ *
+ * Return:void
  */
-void print_alphbet(void)
+void print_alphabet(void)
 {
-	char l = 'a';
-
-	while (l <= 'z')
-	{
-		_putchar(l);
-		l++;
-	}
-	_putchar('\n');
+char c;
+for (c ='a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
 }
