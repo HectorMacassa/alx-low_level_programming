6971fc83f875704460a32b454c4281dd77c04a64
Hector Macassa <cbmacassa@gmail.com>
2023-10-11 14:26:31 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,14 +29,14 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
-                                printf("\n");
-                                break;
-                        }
-                        else
-                        {
-                                printf("%d, ", n);
-                        }
+				print("\n");
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
 }
