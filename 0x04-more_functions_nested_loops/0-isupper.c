d35c80f6998463f5e4b20189bd5c7e5342bb7632
Hector Macassa <cbmacassa@gmail.com>
2023-10-12 06:31:07 -0700

    Is Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,3 +4,11 @@
  * @c: the character being checked
  * Return: 1 for uppercase letter, 0 for anything else
  */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
